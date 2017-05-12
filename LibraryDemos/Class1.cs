4c4853ba7dae5906f5226af4be76551ae0e1be71
Billly <lyy0808@yeah.net>
2017-05-12 16:18:38 +0800

    添加项目文件。

---
+++ LibraryDemos/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDemos
+{
+    public class Class1
+    {
+        public Class1()
+        {
+          
+        }
+
+      
+
+        public static string getString()
+        {
+
+            return Properties.Settings1.Default.ll;
+         
+        }
+
+        public static void SaveThe()
+        {
+           // Properties.LibrarySetting.Default.ISerove = "23232323";
+          
+           // Properties.LibrarySetting.Default.Save();
+            Properties.Settings1.Default.Upgrade();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using LibraryDemos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            lb.Content = Class1.getString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Class1.SaveThe();
+            lb.Content = Class1.getString();
+        }
+    }
+}
